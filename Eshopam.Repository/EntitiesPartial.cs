86058cefca5ff73bb09fd043013721aa04d1b0f3
Drystan Tchamba <Drystan Tchamba@DESKTOP-UKLB86S>
2022-03-07 18:15:22 +0100

    Add project files.

---
+++ Eshopam.Repository/EntitiesPartial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshopam.Repository
+{
+    public partial class User
+    {
+        public User(int id, string username, string password, string fullname, string role)
+            :this()
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Fullname = fullname;
+            Role = role;
+        }
+    }
+
+    public partial class Category
+    {
+        public Category()
+        {
+                
+        }
+
+        public Category(int id, string name, int userId)
+            : this()
+        {
+            Id = id;
+            Name = name;
+            UserId = userId;
+        }
+
+        public Category(int id, string name, int userId, User user)
+            : this(id, name, userId)
+        {     
+            User = user;
+        }
+    }
+}
